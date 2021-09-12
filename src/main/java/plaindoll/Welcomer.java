e3c09abcf90ce7b04cd7e14c7746ebf1eca563d8
Alexander Scherba <scherba.alexander@gmail.com>
2021-09-12 12:29:22 +0300

    add new welcomer method and extend test

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -7,4 +7,5 @@ public class Welcomer{
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
+	public String sayCredo() { return "The hunter must always find his target"; }
 }
